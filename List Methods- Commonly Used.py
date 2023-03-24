906fb33eadb3958ee916ece7831820dacb0ccebe
Brycer0728 <Brycesteele30@gmail.com>
2023-03-23 23:30:22 -0400

    Other List Methods

---
+++ List Methods- Commonly Used.py
@@ -0,0 +1,10 @@
+# Other commonly used List Methods
+basket = ['a','b','c','d','e']
+
+# Index is used to find where a value is in the list
+# print(basket.index(x, y, z))
+# x = value that we are looking for
+# y = is where to put an index value to start your search in the list
+# z = is where to put an index value to end your search in the list
+print(basket.index('d', 0, 4))
+# example above prints '3', in the list 'd' is at the 3rd index
