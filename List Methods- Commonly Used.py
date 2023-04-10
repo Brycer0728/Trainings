ee4b80e80c09623ff26d0dbd0eef5eebc6b119a7
Brycer0728 <Brycesteele30@gmail.com>
2023-04-09 21:41:22 -0400

    ain

--- List Methods- Commonly Used.py
+++ List Methods- Commonly Used.py
@@ -1,5 +1,6 @@
 # Other commonly used List Methods
 basket = ['a','b','c','d','e']
+basket_2 = ['a','x','b','c','d','e','d']
 
 # Index is used to find where a value is in the list
 # print(basket.index(x, y, z))
@@ -16,3 +17,21 @@ print(basket.count('d'))
 # the keyword 'in' lets you know if something is present in another thing
 print('b' in 'basketball')
 # line 17 prints 'True" because the letter 'b' shows up in basketball
+
+# '.sort()' sorts the list, modififes the list, '.sort()' doesn't return anything
+basket_2.sort()
+# printing 'print(basket_2.sort())' wouldn't return anything
+print(basket_2)
+# line 23 prints '['a', 'b', 'c', 'd', 'd', 'e', 'x']'
+
+# 'sorted' is like '.sort()' but it returns something, it doesn't modify the list, it creates a new one
+print(sorted(basket_2))
+# line 28 prints '['a', 'b', 'c', 'd', 'd', 'e', 'x']' because sorted will actually return a value
+
+# '.copy()' allows you to copy a list
+
+# '.reverse()' reverses the order of the list but doesn't sort it. It just reverses the indexs
+# since 'basket_2' is still sorted due to line 28, if we reverse the order it will print in a sorted reverse order
+basket_2.reverse()
+print(basket_2)
+# line 36 printed '['x', 'e', 'd', 'd', 'c', 'b', 'a']'
