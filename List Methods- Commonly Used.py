b9d5bc5f44c6b5b5cd106a2cbad04122e8cab555
Brycer0728 <Brycesteele30@gmail.com>
2023-04-09 21:27:23 -0400

    ain

--- List Methods- Commonly Used.py
+++ List Methods- Commonly Used.py
@@ -8,3 +8,11 @@ basket = ['a','b','c','d','e']
 # z = is where to put an index value to end your search in the list
 print(basket.index('d', 0, 4))
 # example above prints '3', in the list 'd' is at the 3rd index
+
+print(basket.count('d'))
+# line 12 prints '1' because 'd' only shows up in basket once
+# '.count()' is used to see how many times something shows up in a list
+
+# the keyword 'in' lets you know if something is present in another thing
+print('b' in 'basketball')
+# line 17 prints 'True" because the letter 'b' shows up in basketball
